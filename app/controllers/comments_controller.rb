0325415872cbe18e294acfff246c095c33fe8f78
chang <ycw99@hotmail.com>
2016-04-21 15:38:33 +0800

    comments page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,18 @@
 class CommentsController < HomeController
 
   def index
+    respond_to do |format|
+      format.html {
 
+      }
+      format.json {
+        @data = [
+          {id: 1, author: "Pete Hunt", content: "This is one comment"},
+          {id: 2, author: "Jordan Walke", content: "This is *another* comment"}
+        ]
+        render json: @data.to_json
+      }
+    end
   end
 
 end
